15a808b7c59c2c8ffa4b9a9bff889cf1e72883a8
Emelie Ramenberg <ramenberg@gmail.com>
2023-04-03 14:29:10 +0200

    Refaktorerat main.js

--- js/main.js
+++ js/main.js
@@ -294,14 +294,137 @@ function validateForm() {
 	});
 }
 
-// Div mindre funktioner
+// Visar beställda produkter på bekräftelsesidan.
+function renderConfirmationProducts() {
+	const cartItems = localStorage.getItem('fs-cart');
 
-// Funktion för att tömma varukorgen
-function emptyCart() {
-	localStorage.removeItem('fs-cart');
-	window.location.href = 'cart.html';
+	if (!cartItems || cartItems.length === 0) {
+		console.log('Varukorgen är tom');
+		let emptyCart = `
+        <div class="col-12 my-5">
+            <h2 class="text-center">Här var det tomt!</h2>
+            <p class="text-center text-bold"><a href="index.html">Fortsätt handla</a></p>
+        </div>
+    `;
+		$('.before-cart').html(emptyCart);
+	} else {
+		const cart = JSON.parse(cartItems);
+		let checkoutFinished = false;
+
+		if (cart.length === 0) {
+			console.log('Varukorgen är tom');
+		} else {
+			let cartSum = 0;
+			let totalItems = 0;
+
+			// funktion för att räkna ut totala summan av varukorgen
+			function totalSum() {
+				let totalSum = 0;
+				for (let i = 0; i < cart.length; i++) {
+					totalSum += cart[i].price * cart[i].quantity;
+				}
+				return totalSum;
+			}
+			cartSum = totalSum();
+
+			function totalItemsInCart() {
+				let totalItems = 0;
+				for (let i = 0; i < cart.length; i++) {
+					totalItems += cart[i].quantity;
+				}
+				return totalItems;
+			}
+			totalItems = totalItemsInCart();
+
+			if ($('#confirmed-items').length) {
+				// Pre info
+				let preInfo = `
+			<div class="col-7 cart-item-name text-bold">Produkt</div>
+			<div class="cart-item-quantity text-bold">Antal</div>
+			<div class="cart-item-price text-bold">Pris</div>
+			`;
+				$('.pre-info').append(preInfo);
+				console.log('preinfo körs');
+				// Loopa igenom varukorgen och skriv ut varje produkt
+				let singleProduct = '';
+				cart.forEach(function (product) {
+					singleProduct += `
+            
+            <div class="row border-bottom cart-item" data-product-id="${
+							product.id
+						}">
+
+                <div class="col w-100 d-flex justify-content-between px-3">
+                    <div class="row main w-100 py-2 px-3">
+                        <!-- Image -->
+                        <div class="col cart-image d-block">
+                            <img class="img-fluid" src="${product.image}" />
+                        </div>
+                        <!-- Title -->
+                        <div class="cart-content col px-0">
+                            <!-- Quantity -->
+                            <div class="row">
+                                <div class="col cart-content-info d-flex justify-content-between align-items-baseline">
+								<div class="col">
+									<span class="row cart-item-title px-2">${product.title}</span>
+                                    <span class="row single-price text-muted px-2 pt-1">${product.price.toFixed(
+																			2
+																		)} kr / st</span>
+								</div>
+								
+                                    <span class="text-center px-2">${
+																			product.quantity
+																		}</span>
+                                    <span class="col-3 text-bold text-right">${(
+																			product.price * product.quantity
+																		).toFixed(2)} kr
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- Totalpris för varan beroende på antal-->
+                    </div>
+                </div>
+            </div>
+			
+            `;
+				});
+				$('#confirmed-items').append(singleProduct);
+				console.log(cart.length);
+				console.log('körs');
+				// Summeringsinfo
+				let summaryInfo = `
+            <div class="pt-5 mx-2">
+                <div class="row align-items-baseline">
+                    <div class="col">
+                        <p>Totalt</p>
+                    </div>
+                    <div class="col text-end float-end">
+                        <p class="text-right">${totalItems} varor</p>
+                    </div>
+                </div>
+                <div class="row pb-3 border-top">
+                    <div class="col text-bold">Summa</div>
+                    <div class="col text-right text-bold">${cartSum.toFixed(
+											2
+										)} kr</div>
+                </div>
+            </div>
+            `;
+
+				$('.summary').append(summaryInfo);
+				checkoutFinished = true;
+			}
+		}
+		if (checkoutFinished === true) {
+			console.log('checkoutFinished är true');
+		} else {
+			console.log('checkoutFinished är false');
+		}
+	}
 }
 
+// Visar alla produkter i varukorgen.
 function renderCart(cartArray, cartSum, totalItems) {
 	let cart = cartArray;
 	cartSum = cartSum;
@@ -398,7 +521,7 @@ function renderCart(cartArray, cartSum, totalItems) {
 
 			localStorage.setItem('fs-cart', JSON.stringify(cart));
 			if (cart.length === 0) {
-				emptyCart();
+				emptyCartReload();
 			} else {
 				window.location.reload();
 			}
@@ -421,7 +544,7 @@ function renderCart(cartArray, cartSum, totalItems) {
 
 			localStorage.setItem('fs-cart', JSON.stringify(cart));
 			if (cart.length === 0) {
-				emptyCart();
+				emptyCartReload();
 			} else {
 				window.location.reload();
 			}
@@ -456,7 +579,7 @@ function renderCart(cartArray, cartSum, totalItems) {
 	// Töm varukorg
 	$('#empty-cart-button').click(function (e) {
 		e.preventDefault();
-		emptyCart();
+		emptyCartReload();
 	});
 	// Checkout
 	$('#checkout-button').click(function (e) {
@@ -464,6 +587,8 @@ function renderCart(cartArray, cartSum, totalItems) {
 		window.location.href = 'order.html';
 	});
 }
+
+// Visar produkterna på beställningssidan
 function renderCartCheckout(cartArray, cartSum, totalItems) {
 	let cart = cartArray;
 	cartSum = cartSum;
@@ -559,4 +684,54 @@ function renderCartCheckout(cartArray, cartSum, totalItems) {
 		e.preventDefault();
 		window.location.href = 'index.html';
 	});
+}
+
+// Skapar kundinfo för bekräftelsesidan
+function renderCustomerInformation() {
+	const customerInfo = JSON.parse(sessionStorage.getItem('customerInfo'));
+
+	function showCustomerInfo(customer) {
+		const container = $('#confirmation-customer-info');
+		container.html(`
+            <div class="row d-flex justify-content-between">
+                <div class="col-md-12 ps-5">
+                    <small class="text-muted">Adress:</small>
+                    <p>${customer.firstName} ${customer.lastName}<br>
+                    ${customer.address}<br>
+                    ${customer.postalCode} ${customer.city}<br>
+                    ${customer.country}</p>
+                </div>
+                <div>
+                    <small class="text-muted">E-mail:</small>
+                    <p>${customer.email}</p>
+                    <small class="text-muted">Telefon:</small>
+                    <p>${customer.phone}</p>
+                </div>
+            </div>
+            `);
+	}
+	if ($(customerInfo).length) {
+		showCustomerInfo(customerInfo);
+	} else {
+		console.log('Ingen kund vald eller sessionStorage är tom.');
+	}
+}
+
+// Körs på confirmation för att hämta produkter, kundinfo och sedan tömma varukorgen.
+function renderConfirmation() {
+	renderConfirmationProducts();
+	renderCustomerInformation();
+	emptyCartNonReload();
+}
+
+// Div mindre funktioner
+
+// Funktion för att tömma varukorgen
+function emptyCartReload() {
+	localStorage.removeItem('fs-cart');
+	window.location.reload();
+}
+// Utan reload.
+function emptyCartNonReload() {
+	localStorage.removeItem('fs-cart');
 }
